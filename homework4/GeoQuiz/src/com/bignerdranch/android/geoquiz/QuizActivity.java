41442da2562d45652c7ba7aa5729412a93808fd0
lrpaul01 <lrpaul01@louisville.edu>
2013-10-14 00:13:07 -0400

    UpdateCh2

--- homework4/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ homework4/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -5,12 +5,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true),
+	};
+	
+	private int mCurrentIndex = 0;
+	
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue){
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		
+		if (userPressedTrue == answerIsTrue){
+			messageResId = R.string.correct_toast;
+		}else{
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+		.show();
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,9 +55,7 @@ public class QuizActivity extends Activity {
 		mTrueButton.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, 
-						R.string.incorrect_toast, 
-						Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
 		
@@ -31,15 +63,24 @@ public class QuizActivity extends Activity {
 		mFalseButton.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View v){
-				Toast.makeText(QuizActivity.this,
-						R.string.correct_toast, 
-						Toast.LENGTH_SHORT).show();
-				
+				checkAnswer(false);				
 			}
 		});
-	}
 		
-							
+		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+		
+		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+								
+		updateQuestion();
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
